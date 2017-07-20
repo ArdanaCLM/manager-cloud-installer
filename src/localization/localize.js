5fdeca6a913b7d5126b43caa8116b154202fcabc
Jeremy Moffitt <jeremy.moffitt@suse.com>
2017-07-20 11:34:54 -0700

    clean-up formatting

--- src/localization/localize.js
+++ src/localization/localize.js
@@ -3,7 +3,7 @@ import LocalizedStrings from 'react-localization';
 var supportedLangs = ['en', 'ja'];
 var translationData, bundlename, catalog = {};
 
-for(var i = 0; i < supportedLangs.length; i++){
+for (var i = 0; i < supportedLangs.length; i++) {
     bundlename = "./bundles/" + supportedLangs[i] + ".json";
     //require doesn't interpret this as a string correctly unless its converted
     //easiest conversion is to use + ''
@@ -13,13 +13,13 @@ for(var i = 0; i < supportedLangs.length; i++){
 
 //TODO - determine what to do with annotated langs... like en-US
 var strings = new LocalizedStrings(catalog);
-for(i = 0; i < window.navigator.languages.length; i++){
-    if(supportedLangs.indexOf(window.navigator.languages[i]) !== -1){
+for (i = 0; i < window.navigator.languages.length; i++) {
+    if (supportedLangs.indexOf(window.navigator.languages[i]) !== -1) {
         strings.setLanguage(window.navigator.languages[i]);
         break;
     }
 }
 
-export function translate (key, ...args){
+export function translate(key, ...args) {
     return strings.formatString(strings[key], ...args);
 }
