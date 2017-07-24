56e1e625de7a45fa5b669abd7b793aa4407a5678
Jeremy Moffitt <jeremy.moffitt@suse.com>
2017-07-24 13:41:52 -0700

    Add basic localization functionality (#6)
    * Add basic localization functionality
    
    * Removed extraneous en, clarified string conversion for require
    
    * clean-up formatting

---
+++ src/localization/localize.js
@@ -0,0 +1,25 @@
+import LocalizedStrings from 'react-localization';
+
+var supportedLangs = ['en', 'ja'];
+var translationData, bundlename, catalog = {};
+
+for (var i = 0; i < supportedLangs.length; i++) {
+    bundlename = "./bundles/" + supportedLangs[i] + ".json";
+    //require doesn't interpret this as a string correctly unless its converted
+    //easiest conversion is to use + ''
+    translationData = require(bundlename + '');
+    catalog[supportedLangs[i]] = translationData;
+}
+
+//TODO - determine what to do with annotated langs... like en-US
+var strings = new LocalizedStrings(catalog);
+for (i = 0; i < window.navigator.languages.length; i++) {
+    if (supportedLangs.indexOf(window.navigator.languages[i]) !== -1) {
+        strings.setLanguage(window.navigator.languages[i]);
+        break;
+    }
+}
+
+export function translate(key, ...args) {
+    return strings.formatString(strings[key], ...args);
+}
