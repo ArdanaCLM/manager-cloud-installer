5672bf9936b5636826244b1b4fa62973bd0ed234
rarcea <r.arcea@gmail.com>
2017-07-28 18:36:09 +0200

    remove duplicate of server declaration

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var jsonServer = require('json-server');
 var port = process.env.PORT || 3000;
 var server = express()
 
-var server = express();
-
 server.use(express.static("./public"));
 server.use('/api', jsonServer.router('./api/api.json'));
 
