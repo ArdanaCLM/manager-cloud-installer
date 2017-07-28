982c1af074726270f9a4d7db9ce94c22fa3eb036
Jeremy Moffitt <jeremy.moffitt@suse.com>
2017-07-28 10:27:23 -0700

    Updated based on feedback, added new styling, broke out states into separate shared const

---
+++ src/components/StepStateValues.js
@@ -0,0 +1,6 @@
+export const stepStateValues = {
+    'notdone': 0,
+    'done': 1,
+    'inprogress': 2,
+    'error': 3
+}
--- src/components/WizardProgress.js
+++ src/components/WizardProgress.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
+import { stepStateValues } from './StepStateValues.js';
 //import '../App.css';
 //import { translate } from './localization/localize.js';
 
 class WizardProgress extends Component {
     render()
     {
-	    var stateBubbles = this.props.steps.map(function(item,index){
-		    console.log(item.keys);
-		    console.log(item.state);
-		    console.log(index);
-		    if(item.state === 0){
-		    	return (
-			    <span key={index} className="progress notdone"/>
-		    	);
-		    } else {
-			return (
-			    <span key={index} className="progress done"/>
-			);
-		    }
-	    });
-	    return(
-		<div>
-		    {stateBubbles}
-		    <hr/>
-		    On step {this.props.currentStep}
-	        </div>
-	    )
+        var stateBubbles = this.props.steps.map(function(item,index){
+            if(item.state === stepStateValues.done){
+                return (
+                    <span key={index} className="progress done"/>
+                );
+            } else if(item.state === stepStateValues.inprogress){
+                return (
+                    <span key={index} className="progress inprogress"/>
+                    );
+            } else if(item.state === stepStateValues.error){
+                return (
+                    <span key={index} className="progress error"/>
+                    );
+            } else {
+                return (
+                    <span key={index} className="progress notdone"/>
+                );
+            }
+        });
+        return(
+            <div className="stateOuterWrapper">
+                <div className="stateLineWrapper">
+                    <span>
+                        {stateBubbles}
+                    </span>
+                </div>
+            </div>
+        )
     }
 }
 
