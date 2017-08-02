9be232f6de30d687d71e7627f4cbcb04334fad18
rarcea <r.arcea@gmail.com>
2017-08-02 19:06:23 +0200

    add hot loading as default for npm start

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './index.js',
+  entry: [
+    'react-hot-loader/patch',
+    './index.js'
+  ],
   output: {
     filename: 'public/bundle.js'
   },
   module: {
     loaders: [
       {
+        test: /\.js$/,
         loader: 'babel-loader',
         exclude: /(node_modules|server.js)/,
         query: {
           cacheDirectory: true,
           presets: ['react', 'es2015', 'stage-2']
         }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader/webpack'
       },
       {
         test: /\.css$/,
