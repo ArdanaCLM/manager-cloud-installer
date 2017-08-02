6c07bf71a3b09e069195ae34e49d8fbdc4cbae7c
Roman Arcea <sidthe@users.noreply.github.com>
2017-08-02 19:58:30 +0200

    add hot loading as default for npm start (#23)

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+import { AppContainer } from 'react-hot-loader';
 import React from "react";
 import ReactDOM from 'react-dom';
+
 import Deployer from './src/Deployer';
 import './src/Deployer.css';
 
-ReactDOM.render(<Deployer />, document.getElementById('root'));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(Deployer);
+
+
+if (module.hot) {
+  module.hot.accept('./src/Deployer', () => {
+    const Deployer = require('./src/Deployer.js').default;
+    render(Deployer) });
+}
