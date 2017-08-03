0604a8696eac03454a5282593199f708bb825667
Gary Smith <garysmith123@gmail.com>
2017-08-03 15:23:09 -0700

    Create subcomponents in ValidateConfigFiles (#29)
    Create separate components within ValidateConfigFiles for handling the
    review list and the edit page.  Establish state, props passing between
    the various components.

--- src/pages/ValidateConfigFiles.js
+++ src/pages/ValidateConfigFiles.js
@@ -4,11 +4,63 @@ import { translate } from '../localization/localize.js';
 import GenericPlaceHolder from './GenericPlaceHolder';
 import { ActionButton } from '../components/Buttons.js';
 
+const INVALID = 0
+const VALID = 1
+const UNKNOWN = -1
+
+class EditFile extends Component {
+
+  render() {
+    return (
+      <div className='heading'>
+        {translate('edit.config.files.heading', this.props.file.description, this.props.file.name)}
+        <pre>
+          {this.props.file.name}
+          {this.props.file.description}
+        </pre>
+        <ActionButton
+          displayLabel='Done'
+          clickAction={() => this.props.onClick()}/>
+      </div>
+    );
+  }
+}
+
+class DisplayFileList extends Component {
+
+  render() {
+    var list = this.props.files.map((file, index) => {
+      return (<li key={index}>
+        <a href="#" onClick={() => this.props.onEditClick(file)}>{file.description}</a>
+      </li>)
+    });
+
+    return (
+      <div>
+        <div className='heading'>
+          {translate('validate.config.files.heading')}
+        </div>
+        <div>{this.props.valid}</div>
+        <div className='validateConfigFiles'>
+          <ul>{list}</ul>
+        </div>
+        <div>
+          <ActionButton
+            displayLabel={translate('validate.config.files.validate')}
+            clickAction={() => this.props.onValidateClick()}/>
+        </div>
+      </div>
+    );
+  }
+}
+
 class ValidateConfigFiles extends GenericPlaceHolder {
   constructor() {
     super();
     this.state = {
       configFiles: [],
+      valid: UNKNOWN,
+      editingFile: ''
     }
 
     // retrieve a list of yml files
@@ -16,41 +68,63 @@ class ValidateConfigFiles extends GenericPlaceHolder {
     fetch('http://localhost:8080/configFiles')
       .then(response => response.json())
       .then((responseData) => {
-        var files = responseData.map((fileObj) => {return fileObj.description});
+        // var files = responseData.map((fileObj) => {return fileObj.description});
+        console.log(responseData);
         this.setState({
-          configFiles: files
+          configFiles: responseData
         });
       });
   }
 
+  isError() {
+    return(this.state.valid != VALID)
+  }
+
+  editFile(file) {
+    this.setState({editingFile: file});
+  }
+
+  doneEditingFile() {
+    this.setState({editingFile: ''});
+    this.setState({valid: UNKNOWN});
+  }
+
+  validateModel() {
+    console.log('validate model')
+  }
+
+  renderBody() {
+    console.log('renderBody')
+    if (this.state.editingFile === '') {
+      return (
+        <DisplayFileList
+          files={this.state.configFiles}
+          onValidateClick={() => this.validateModel()}
+          onEditClick={(file) => this.editFile(file)}
+          valid={this.state.valid}
+        />);
+    } else {
+      return (
+        <EditFile
+          file={this.state.editingFile}
+          onClick={() => this.doneEditingFile()}
+          setValid={(val) => this.setValid(val)}
+        />
+      );
+    }
+  }
+
   render() {
-    var list = this.state.configFiles.map((file, index) => {
-      return (<li key={index}>{file}</li>)
-    });
-
     return (
-      <div>
-        <div className='wizardContentPage'>
-          <div className='heading'>
-            {translate('validate.config.files.heading')}
-          </div>
-
-          <div className='validateConfigFiles'>
-            <ul>{list}</ul>
-          </div>
-        </div>
-        <div>
-          <ActionButton
-            displayLabel={translate('validate.config.files.validate')}
-            clickAction={() => alert('click')}/>
-        </div>
+      <div className='generic-container'>
+        {this.renderBody()}
         {this.renderNavButtons()}
       </div>
     );
   }
 
-  validateModel() {
-
+  setValid(state) {
+    this.state.valid = state;
   }
 }
 
