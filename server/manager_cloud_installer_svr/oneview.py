4b7a4f6f5e1e28b0ca9afae2c296e3b720a8238e
Gary Smith <gary.smith@suse.com>
2017-08-04 13:12:33 -0700

    Implement SUSE Manager interface
    Get SUSE Manager interface working, including option for insecure SSL.
    Refactor server into separate files, one for each remote system type
    (ardana, oneview, etc.).

---
+++ server/manager_cloud_installer_svr/oneview.py
@@ -0,0 +1,26 @@
+# import config.config as config
+from flask import Blueprint
+from flask import request
+from . import util
+
+bp = Blueprint('oneview', __name__)
+
+# ONEVIEW_URL = config.get("oneview", "url")
+
+"""
+Calls to HPE OneView
+"""
+
+
+@bp.route("/api/v1/ov/servers")
+def ov_server_list():
+    # For now, use only the JSON_SERVER
+    url = util.JSON_SERVER + "/servers"
+    return util.forward(url, request)
+
+
+@bp.route("/api/v1/ov/servers/<id>")
+def ov_server_details(id):
+    # For now, use only the JSON_SERVER
+    url = util.JSON_SERVER + "/servers/" + id
+    return util.forward(url, request)
