bee7bad0af180ada4ec035dd763c2ca17367f2f7
Gary Smith <gary.smith@suse.com>
2017-08-09 16:21:52 -0700

    Save progress in server

--- server/manager_cloud_installer_svr/ui.py
+++ server/manager_cloud_installer_svr/ui.py
@@ -1,6 +1,9 @@
+from flask import abort
 from flask import Blueprint
+from flask import jsonify
 from flask import request
-from . import util
+import json
+import os
 
 bp = Blueprint('ui', __name__)
 
@@ -8,10 +11,34 @@ bp = Blueprint('ui', __name__)
 Calls handled locally to support the UI
 """
 
+# This location should be in a config file
+progress_file = os.path.normpath(os.path.join(os.path.dirname(__file__),
+                                              "progress.json"))
 
-@bp.route("/api/v1/progress", methods=['GET', 'POST', 'PUT', 'DELETE'])
-def progress():
 
-    # For now, use only the JSON_SERVER
-    url = util.JSON_SERVER + "/installprogress"
-    return util.forward(url, request)
+@bp.route("/api/v1/progress", methods=['GET'])
+def get_progress():
+
+    contents = ''
+    try:
+        with open(progress_file) as f:
+            contents = json.load(f)
+
+    except IOError:
+        pass
+
+    return jsonify(contents)
+
+
+@bp.route("/api/v1/progress", methods=['POST'])
+def save_progress():
+
+    data = request.get_json()
+
+    try:
+        with open(progress_file, "w") as f:
+            json.dump(data, f)
+
+        return 'Success'
+    except Exception:
+        abort(400)
