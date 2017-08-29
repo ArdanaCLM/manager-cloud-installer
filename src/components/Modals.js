8efe9f6f42c26ab6b1d2d9d38f2a3846dff5782e
gugl <gloria.fang.gu@suse.com>
2017-08-29 15:03:41 -0700

    Added server landing page - draft 1
    First step to refactor the assign server role page to match the latest
    mock design

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -59,7 +59,94 @@ class YesNoModal extends ConfirmModal {
   }
 }
 
+class BaseInputModal extends Modal {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      isDoneDisabled: this.props.isDoneDisabled
+    };
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      showModal: newProps.show,
+      isDoneDisabled: newProps.isDoneDisabled
+    });
+  }
+
+  renderFooter() {
+    if(this.props.footer) {
+      return this.props.footer;
+    }
+    else {
+      return (
+        <div>
+          <ActionButton
+            hasNext
+            clickAction={this.props.cancelAction} displayLabel={translate('cancel')}/>
+          <ActionButton
+            isDisabled={this.state.isDoneDisabled}
+            clickAction={this.props.doneAction} displayLabel={translate('done')}/>
+        </div>
+      );
+    }
+  }
+  render() {
+    return (
+      <Modal
+        className='modals' show={this.state.showModal}
+        onHide={this.props.cancelAction} backdrop={'static'}>
+        <Modal.Header>
+          <Modal.Title className='title'>{this.props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {this.props.body}
+        </Modal.Body>
+        <Modal.Footer>
+          {this.renderFooter()}
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+class ConnectionInputModal extends BaseInputModal {
+  constructor(props) {
+    super(props);
+  }
+
+  renderFooter() {
+    return (
+      <div>
+        <ActionButton
+          hasNext
+          clickAction={this.props.cancelAction} displayLabel={translate('cancel')}/>
+        <ActionButton
+          hasNext
+          clickAction={this.props.testAction} displayLabel={translate('test')}/>
+        <ActionButton
+          is
+          Disabled={this.props.isDoneDisabled}
+          clickAction={this.props.doneAction} displayLabel={translate('done')}/>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <BaseInputModal
+        cancelAction={this.props.cancelAction} testAction={this.props.testAction}
+        doneAction={this.props.doneAction}
+        show={this.state.showModal} title={this.props.title}
+        body={this.props.body} footer={this.renderFooter()}/>
+    );
+  }
+}
+
 module.exports = {
   ConfirmModal: ConfirmModal,
-  YesNoModal: YesNoModal
+  YesNoModal: YesNoModal,
+  BaseInputModal: BaseInputModal,
+  ConnectionInputModal: ConnectionInputModal
 };
---
+++ src/components/ServerUtils.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import Collapsible from 'react-collapsible';
+import '../Deployer.css';
+import { translate } from '../localization/localize.js';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+  }
+
+  handleFilterTextInputChange(e) {
+    e.preventDefault();
+    this.props.filterAction(e.target.value);
+  }
+
+  render() {
+    let searchPlaceholder = translate('placeholder.search.server.text');
+    return (
+      <div className='search-container'>
+        <span className='search-bar'>
+          <input className='rounded-box'
+            type="text" placeholder={searchPlaceholder}
+            value={this.props.filterText} onChange={this.handleFilterTextInputChange}/>
+        </span>
+        <span className='glyphicon glyphicon-search search-icon'></span>
+      </div>
+    );
+  }
+}
+
+class ServerRolesAccordion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accordionPosition: 0
+    };
+    this.handleTriggerClick = this.handleTriggerClick.bind(this);
+  }
+
+  //TODO temp implment...need table with drag and drop
+  renderServerTable(servers) {
+    let items = [];
+    servers.forEach((server, idx) => {
+      let item = <div key={idx}>{server.name}</div>;
+      items.push(item);
+    });
+    return items;
+  }
+
+  handleTriggerClick(position, role) {
+    this.setState({accordionPosition: position});
+    this.props.clickAction(role);
+  }
+
+  renderSections() {
+    let sections = this.props.serverRoles.map((role, idx) => {
+      let modelCount = role.minCount !== undefined ? role.minCount : role.memberCount;
+      let optionDisplay =
+        role.name + ' (' + role.serverRole + ' ' + role.servers.length + '/' + modelCount + ')';
+      return (
+        <Collapsible
+          open={idx === this.state.accordionPosition}
+          trigger={optionDisplay} key={role.name}
+          handleTriggerClick={() => this.handleTriggerClick(idx, role)}
+          value={role.serverRole}>
+          {this.renderServerTable(role.servers)}
+        </Collapsible>
+      );
+    });
+
+    return sections;
+  }
+
+  render() {
+    return (
+      <div className='roles-accordion'>
+        {this.renderSections()}
+      </div>
+    );
+  }
+}
+
+module.exports = {
+  SearchBar: SearchBar,
+  ServerRolesAccordion: ServerRolesAccordion
+};
