9b516a2d43dd3b210ec9c4c36f36ae3d9dbc818c
Gary Smith <gary.smith@suse.com>
2017-09-29 15:52:00 -0700

    Sort role assignment, role summary pages
    Sort the servers by name (or id) within each role, and sort the names
    alphabetically.  Create Sort.js with alphabetically()

--- src/pages/ServerRoleSummary.js
+++ src/pages/ServerRoleSummary.js
@@ -4,6 +4,7 @@ import { translate } from '../localization/localize.js';
 import BaseWizardPage from './BaseWizardPage.js';
 import CollapsibleTable from '../components/CollapsibleTable.js';
 import { List, Map } from 'immutable';
+import { alphabetically } from '../utils/Sort.js';
 
 class ServerRoleSummary extends BaseWizardPage {
 
@@ -11,17 +12,21 @@ class ServerRoleSummary extends BaseWizardPage {
     super(props);
   }
 
+  byServerNameOrId = (a,b) => {
+    return alphabetically(a.get('name') || a.get('id'), b.get('name') || b.get('id'));
+  }
+
   formatServerObjects = () => {
 
     const servers = this.props.model.getIn(['inputModel','servers']);
-    
+
     // Create a map of role names to list of servers in each, e.g.
     //   { 'COMPUTE':[{name:'one',...},{name:'two',...},  'CONTROLLER': [...]}
     let groupMap = Map();
-    servers.forEach(server => {
-      groupMap = groupMap.update(server.get('role'), 
+    servers.sort((a,b) => this.byServerNameOrId(a,b)).forEach(server => {
+      groupMap = groupMap.update(server.get('role'),
         new List(),           // create a new list if role is not in groupMap
-        list => list.push(    // append this servers to the role's list
+        list => list.push(    // append this server to the role's list
           new Map({
             'name': server.get('name') || server.get('id'),
             'ipAddr': server.get('ip-addr'),
@@ -32,12 +37,12 @@ class ServerRoleSummary extends BaseWizardPage {
     });
 
     // Convert the map to a list of objects and return it, e.g.
-    //  [ {groupName:'COMPUTE', members:[{name:'one',...},{name:'two',...}, 
+    //  [ {groupName:'COMPUTE', members:[{name:'one',...},{name:'two',...},
     //    {groupName:'CONTROLLER', members:[..]}... ]
     return groupMap.keySeq().sort()         // get a sorted list of keys
       .map(g => new Map({
         'groupName': g,
-        'members': groupMap.get(g)})) 
+        'members': groupMap.get(g)}))
       .toJS();                              // return as JavaScript objects
   }
 
