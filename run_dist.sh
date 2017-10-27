9d7935cbedde20f0b2e1293f2b31f68eb772fa52
Jeremy Moffitt <jeremy.moffitt@suse.com>
2017-10-27 11:06:32 -0700

    Build whole venv, clean-up package scripts (#182)
    * Clean-up font imports and URL reads from config helper, add notes on apache deployment attempts
    
    * clean-up improper file change from previous commit
    
    * Add ability to run minified UI from the flask server that hosts the shim layer, and a script to set it up and get it running
    
    * clean up some commit oddities
    
    * add config file with base urls, expand lint ignore list to include venv and post-dist directory
    
    * fix feedback items, add license info and clean-up shell scripts
    
    * update name of the log file to install.log
    
    * help if I update the comment refering to the log file name too
    
    * Move running dist entirely into venv, package venv as tarball, add instructions
    
    * clean-up script leftovers, remove deprecated script
    
    * update instructions to use tgz and the venv copy of python

---
+++ run_dist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# package the server and UI components into the right folder
+./build_dist.sh
+
+# activate the venv
+source manager_cloud_installer_server_venv/bin/activate
+
+# start the flask server via main.py
+./manager_cloud_installer_server_venv/bin/python ./manager_cloud_installer_server_venv/lib/python2.7/site-packages/cloudinstaller/main.py
