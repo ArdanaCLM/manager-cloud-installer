bdbeec3230b709834cc6508c45c89c3b60e8645d
Julie Gravel <julie.gravel@suse.com>
2017-11-07 16:58:24 -0800

    SCRD-1464 Minor updates to Server Groups tab (#204)
    * SCRD-1464 Update Server Group tab
    
    - change delete icon from minus to trash
    - remove 'View Detail' menu
    - add section titles "Networks:" and "Server Groups:" in edit mode
    
    * SCRD-1464 - Minor updates to Server Groups tab
    
    fix lints complaints
    
    * SCRD-1464 Minor updates to Server Groups tab
    
    change resouces ("Network - None", "Server Group - None") in dropdown menu to "None"

--- src/pages/ServerRoleSummary/ServerGroupDetails.js
+++ src/pages/ServerRoleSummary/ServerGroupDetails.js
@@ -102,11 +102,11 @@ class ServerGroupDetails extends Component {
 
   render() {
     const networkDefaultOption = {
-      label: translate('details.network.none'),
+      label: translate('none'),
       value: ''
     };
     const serverGroupDefaultOption = {
-      label: translate('details.server.group.none'),
+      label: translate('none'),
       value: ''
     };
     const networks = this.props.model.getIn(['inputModel','networks'])
@@ -135,9 +135,11 @@ class ServerGroupDetails extends Component {
               inputValue={this.state.name} inputName='name' inputType='text' {...extraProps}
               inputAction={this.handleInputLine} inputValidate={UniqueNameValidator}
               autoFocus={true}/>
+            <div className='details-group-title'>{translate('edit.networks') + ':'}</div>
             <InlineAddRemoveDropdown name='networks' options={networks}
               values={this.state.networks} defaultOption={networkDefaultOption}
               sendSelectedList={this.getSelectedNetworks}/>
+            <div className='details-group-title'>{translate('edit.server.groups') + ':'}</div>
             <InlineAddRemoveDropdown name='serverGroups' options={serverGroups}
               values={this.state.serverGroups} defaultOption={serverGroupDefaultOption}
               sendSelectedList={this.getSelectedServerGroups} exceptions={exceptions}/>
