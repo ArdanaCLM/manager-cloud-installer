558bdb2ac8eeb4ed7f1b5256b35f173c138a688b
jmoffitt <jeremy.moffitt@suse.com>
2017-11-08 10:09:03 -0800

    Default shim url to be the same host on port 8081

--- src/utils/ConfigHelper.js
+++ src/utils/ConfigHelper.js
@@ -13,8 +13,8 @@
 * limitations under the License.
 **/
 var appConfigs = {
-  'shimurl': 'http://localhost:8081',
-  'deployserviceurl': 'http://localhost:9085',
+  'shimurl': window.location.protocol + '//' + window.location.hostname + ':8081',
+  'deployserviceurl': window.location.protocol + '//' + window.location.hostname + ':9085',
   //remove this when release
   'dev': true
 };
