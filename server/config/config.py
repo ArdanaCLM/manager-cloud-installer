b745707bd1726e91191f8c7873600b6f9ffa813c
Jack Choy <jack.likes.to.code@gmail.com>
2017-12-04 14:02:50 -0800

    remove unused import

--- server/config/config.py
+++ server/config/config.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from ConfigParser import SafeConfigParser
-from ConfigParser import NoSectionError
 import logging
 import os
 
